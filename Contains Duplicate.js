bce831e24a3df463c5d6ef019db1ff9ad837a8fd
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-11-16 21:57:22 +0530

    Update Contains Duplicate.js

--- Contains Duplicate.js
+++ Contains Duplicate.js
@@ -1,1 +1,9 @@
-
+var containsDuplicate = function(nums) {
+    for(let i = 0 ; i < nums.length ; i++){
+        for(let j = i+1 ; j< nums.length ; j++){
+            if(nums[i] === nums[j]) return true
+        }
+        
+    }
+    return false
+};
