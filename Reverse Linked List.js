2e9c33e470bf4c264930e59d6257e454227b5dde
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-11-21 16:10:30 +0530

    Update Reverse Linked List.js

--- Reverse Linked List.js
+++ Reverse Linked List.js
@@ -1,1 +1,15 @@
+//linkedlist has curr, prev, and next for nodes
 
+reverseLinkedlist(head){
+  let curr = head
+  let prev = null
+  let next = null
+  while(curr !== null){
+    next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  head = prev 
+  return node
+}
