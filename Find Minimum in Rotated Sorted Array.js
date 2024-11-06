ea06a1ddadb55ea9322186613ac7981872fe75a1
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2024-11-06 18:45:25 +0530

    Update Find Minimum in Rotated Sorted Array.js

--- Find Minimum in Rotated Sorted Array.js
+++ Find Minimum in Rotated Sorted Array.js
@@ -13,3 +13,8 @@ Example 3:
 Input: nums = [11,13,15,17]
 Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+
+
+var findMin = function(nums) {
+    return Math.min(...nums)
+};
